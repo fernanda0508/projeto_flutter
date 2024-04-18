474334f8983d89671692255ee421b05421f1f91e
ian <ianvictorandrade@hotmail.com>
2024-04-18 16:05:33 -0400

    wip: alteração na tela de detalhes

--- lib/screens/propostas/proposta_detail_screen.dart
+++ lib/screens/propostas/proposta_detail_screen.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import '../../models/proposta.dart';
 
-class DetalhesPropostaScreen extends StatelessWidget {
+class PropostaDetailScreen extends StatelessWidget {
   final Proposta proposta;
 
-  const DetalhesPropostaScreen({required this.proposta});
+  const PropostaDetailScreen({Key? key, required this.proposta}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(proposta.ementa),
+        backgroundColor: Colors.blue, // Cor de fundo da AppBar
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Detalhes da Proposta',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
-              ),
+            Text(
+              'Autor: ${proposta.autor}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8.0),
-            Text('Tipo: ${proposta.siglaTipo}, Número: ${proposta.numero}/${proposta.ano}'),
-            // Adicione mais informações da proposta conforme necessário
+            SizedBox(height: 8),
+            Text('Data de Apresentação: ${proposta.dataApresentacao}'),
+            SizedBox(height: 8),
+            Text('Ementa: ${proposta.ementa}'),
+            SizedBox(height: 8),
+            Text('Situação: ${proposta.situacao}'),
+            SizedBox(height: 8),
+            Text('Autores: ${proposta.autores}'),
+            // Adicione mais detalhes da proposta conforme necessário
           ],
         ),
       ),
